3add1b11a47bd0fd6d6b08b3cd75b2c6a867a8bf
Charlie <iamcharliecohen@gmail.com>
2018-07-14 16:05:04 -0400

    galaga 0.1

--- Examples/Galaga/sketch.js
+++ Examples/Galaga/sketch.js
@@ -1,33 +1,57 @@
 var player;
 var pellet = [];
+var enemies = [];
+var particles = [];
 
 function setup() {
-    createCanvas(400, 300);
+    createCanvas(400, 150 * PI);
     player = new Ship();
+    for (var i = 0; i < 10; i++) {
+        enemies[i] = new Enemy(random(width), -random(height))
+    }
 }
 
 function draw() {
     background(0);
+    for (var i = pellet.length - 1; i >= 0; i--) {
+        pellet[i].update();
+        if (pellet[i].y <= 0) {
+            pellet.splice(i, 1);
+        }
+        for (var j = enemies.length - 1; j >= 0; j--) {
+            if (pellet[i].hit(enemies[j])) {
+                pellet.splice(i, 1);
+                enemies[j].destroy();
+                enemies.splice(j, 1);
+                break;
+            }
+        }
+    }
 
+    for (var i = 0; i < enemies.length; i++) {
+        enemies[i].update();
+        if(enemies[i].y - enemies[i].r >= height){
+            //enemies[i] = new Enemy(random(width), -random(height));
+            enemies.splice(i,1);
+        }
+    }
+    for (var i = pellet.length - 1; i >= 0; i--) {        
+        pellet[i].render();        
+    }
+    for (var i = 0; i < enemies.length; i++) {
+        enemies[i].render();
+    }
     player.update();
     player.render();
-
-    console.log(pellet);
-    for (var i = pellet.length - 1; i >= 0; i--) {
-        
-        pellet[i].update();
-        pellet[i].render();if(pellet[i].y <= 0){
-            pellet.splice(i,1);
-        }
-        
-
-    }
 }
 
 function keyPressed() {
     if (keyCode === LEFT_ARROW) { player.holdLeft = true; }
     if (keyCode === RIGHT_ARROW) { player.holdRight = true; }
     if (keyCode === 32) { player.shoot = true; }
+    if (keyCode === 49) { player.gunMode = 'lineGun' }
+    if (keyCode === 50) { player.gunMode = 'sineGun' }
+    if (keyCode === 51) { player.gunMode = 'rainbowGun' }
 }
 
 function keyReleased() {
@@ -51,6 +75,8 @@ class Ship {
         this.wingWidth = 10;
         this.noseHeight = 5;
         this.color = [255, 255, 255]
+
+        this.gunMode = 'lineGun';
     }
 
     render() {
@@ -74,15 +100,18 @@ class Ship {
         triangle(-this.shipWidth / 2, 0,
             0, -this.noseHeight,
             this.shipWidth / 2, 0)
+        stroke(0);
+        strokeWeight(4);
+        point(0, 0);
         pop();
     }
 
     update() {
-        if(this.x > width - 50){
+        if (this.x > width - 50) {
             this.holdRight = false;
         }
-        
-        if(this.x < 50){
+
+        if (this.x < 50) {
             this.holdLeft = false;
         }
 
@@ -92,32 +121,100 @@ class Ship {
         if (this.holdRight) {
             this.x += 1.8;
         }
-        
-        if(this.shoot === true){
-            pellet.push(new Pellet(this.x, this.y));            
-            console.log(pellet[pellet.length-1]);
-            this.shoot = false; 
+
+        if (this.shoot === true) {
+            pellet.push(new Pellet(this.x, this.y, this.gunMode));
+            this.shoot = false;
         }
     }
 }
 
 class Pellet {
+    constructor(x, y, type) {
+        this.x = x;
+        this.startX = x;
+        this.y = y;
+        this.startY = y;
+        this.v = -3;
+        this.r = 25;
+        this.color = [255, 255, 255]
+
+        this.gunType = type || 'lineGun'
+        this.colorMix = 0
+
+        this.amplitude = 100;
+        this.omega = 3;
+        this.phase = 0;
+
+    }
+
+    render() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.r * 3 / 5, this.r);
+    }
+
+    update() {
+        switch (this.gunType) {
+            case 'lineGun':
+                this.x = this.startX;
+                break;
+            case 'sineGun':
+                angleMode(DEGREES);
+                this.x = this.startX + this.amplitude * sin(this.omega * (this.y - this.startY) + this.phase);
+                break;
+            case 'rainbowGun':
+                if (this.colorMix < 1) {
+                    this.color = [random(255), random(255), random(255)];
+                    this.colorMix += 1;
+                }
+                break;
+        }
+        this.y += this.v;
+    }
+
+    hit(other) {
+        var d = dist(this.x, this.y, other.x, other.y);
+        if (d < other.r) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+}
+
+class Enemy {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.v = -10;
-        this.r = 10;
+        this.r = 40;
+        this.v = 1;
+
     }
 
     render() {
         fill(255);
-        ellipse(this.x, this.y, this.r*3/5, this.r);
+        ellipse(this.x, this.y, this.r * 2, this.r * 2);
     }
 
     update() {
+        this.y += this.v;
+    }
 
-        this.y += this.v
-
-
+    destroy(){
+        particles.push(new particleField(this.x, this.y, this.v, this.r));
     }
 }
+
+class particleField{
+    constructor(x,y,v,r){
+        this.x = x;
+        this.y = y;
+        this.v = v;
+        this.r = r;        
+    }
+
+}
+
